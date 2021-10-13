ed907eac20d5fa105c85fc50c587d6746d77fc78
naxrevlis <me@naxrevlis.com>
2021-10-13 18:06:11 +0300

    formatting the code

--- telegram_sender/main.py
+++ telegram_sender/main.py
@@ -1,13 +1,13 @@
-from uvicorn import config
 import config_loader as config_loader
+import routes
 import uvicorn
 from fastapi import FastAPI
-import routes
+from uvicorn import config
 
 app = FastAPI(title="Telegram Sender")
 app.include_router(routes.router)
 app.add_route("send")
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     uvicorn.run(app, port=config.get(config_loader.WEB_PORT, host=config.get(config_loader.WEB_HOST)))

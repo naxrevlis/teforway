2db1609adf324a32e2b31f2bb858b485db383504
naxrevlis <me@naxrevlis.com>
2021-11-04 00:03:33 +0300

    fixed typo

--- telegram_sender/routes.py
+++ telegram_sender/routes.py
@@ -14,8 +14,8 @@ async def send_message(request: Request, message: str, chat_id: str) -> JSONResp
     message = jsonable_encoder(message)
     # TODO: Add security check for message
     try:
-        responce = await bot.send_message(chat_id=chat_id, text=message)
-        print(responce.text)
+        response = await bot.send_message(chat_id=chat_id, text=message)
+        print(response.text)
         return JSONResponse(status_code=status.HTTP_200_OK, content=message)
     except Exception as e:
         raise JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=str(e))
